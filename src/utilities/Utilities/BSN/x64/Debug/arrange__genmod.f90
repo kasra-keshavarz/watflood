46f442aa5d04ef7dd79230f3c2106beda76706c3
Kasra Farmer <kasra.keshavarz@usask.ca>
2023-10-25 18:42:23 -0400

    Backing up

--- src/utilities/Utilities/BSN/x64/Debug/arrange__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Sep  5 13:07:59 2023
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE ARRANGE__genmod
-          INTERFACE 
-            SUBROUTINE ARRANGE
-            END SUBROUTINE ARRANGE
-          END INTERFACE 
-        END MODULE ARRANGE__genmod
--- utilities/Utilities/BSN/x64/Debug/non_ca__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Sep  5 13:07:59 2023
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE NON_CA__genmod
-          INTERFACE 
-            SUBROUTINE NON_CA
-            END SUBROUTINE NON_CA
-          END INTERFACE 
-        END MODULE NON_CA__genmod
